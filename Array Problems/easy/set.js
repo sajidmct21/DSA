5dd81070312698a80569466002b4e2e2f652a94e
sajid yaqub <sajidmct21@gmail.com>
2024-12-30 12:30:42 +0500

    Set Method is added

---
+++ Array Problems/easy/set.js
@@ -0,0 +1,16 @@
+let arr = [10, 20, 30, 20, 50, 40, 50, 10];
+
+// 1-Delete Duplicate 
+// let res = Array.from(new Set(arr));
+// let s = new Set(arr)
+// let res = [...s]
+// console.log(res);
+
+// 2-Check if element is present or not?
+// let s = new Set(arr);
+// if(s.has(10)){
+//     console.log('Present');
+// }
+// else{
+//     console.log('not Present');
+// }
